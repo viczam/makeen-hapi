6832a63d1fdc0828a289edc1695d7e870584860e
Victor Zamfir <zamfir.victor@gmail.com>
2017-03-21 13:44:35 +0200

    fix password recovery flow

--- packages/mailer/src/views/ForgotPassword.jsx
+++ packages/mailer/src/views/ForgotPassword.jsx
@@ -1,28 +1,24 @@
 import React, { PropTypes } from 'react';
 import Layout from './Layout';
 
-const Hello = ({ user, app }) => (
+const ForgotPassword = ({ user, resetPassword }) => (
   <Layout>
-    <h1>Hello!</h1>
+    <h1>Hello {user.name}!</h1>
     <p>Your forgot you password.</p>
     <p>
       Click {' '}
-      <a href={`${app.client}/change-password/${user.resetPassword.token}`}>here</a>
+      <a href={`http://localhost:3003/change-password/${resetPassword.token}`}>here</a>
       {' '} to recover your password.
     </p>
     <p>ktxbye!</p>
   </Layout>
 );
 
-Hello.propTypes = {
-  user: PropTypes.shape({
-    resetPassword: PropTypes.shape({
-      token: PropTypes.string.isRequired,
-    }),
-  }).isRequired,
-  app: PropTypes.shape({
-    client: PropTypes.string.isRequired,
+ForgotPassword.propTypes = {
+  user: PropTypes.object.isRequired,
+  resetPassword: PropTypes.shape({
+    token: PropTypes.string.isRequired,
   }).isRequired,
 };
 
-export default Hello;
+export default ForgotPassword;
