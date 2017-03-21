235b9f826ca8af1ce7f0abe22e3fd1de80c8aa82
Victor Zamfir <victor.zamfir@bengroup.com>
2017-03-21 21:49:40 +0200

    small corrections

--- packages/user/src/handlers/authenticatedUser.js
+++ packages/user/src/handlers/authenticatedUser.js
@@ -1,7 +1,7 @@
 import { ObjectID as objectId } from 'mongodb';
 
 export default async function (request, reply) {
-  const UserRepository = request.serviceBus.extractRepository('user.User');
+  const { UserRepository } = request.server.plugins['makeen-user'];
   const userId = objectId(request.auth.credentials.id);
   const user = await UserRepository.findById(userId);
 
