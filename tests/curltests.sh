81c27b88a85c2a4c582b879f0ae5a1bbe3093b9a
Nick <nick@wirestorm.us>
2017-03-30 08:49:07 -0700

    finding open ports

--- tests/curltests.sh
+++ tests/curltests.sh
@@ -3,6 +3,7 @@ set -ea
 
 docker ps 
 echo "Docker Host: ${DOCKER_HOST}"
+sudo lsof -i 
 #curl -v http://${DOCKER_HOST}:3000/documentation
 #curl -s http://localhost:3000/documentation | grep window.swaggerUi
 
