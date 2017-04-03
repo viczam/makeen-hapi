c709e2c1ae64132480a62617ea10adbd21314896
Nick <nick@wirestorm.us>
2017-04-03 11:47:44 -0700

    added az environments

--- ci-scripts/helper-functions.sh
+++ ci-scripts/helper-functions.sh
@@ -15,3 +15,20 @@ get_fulldockertag () {
 		echo ${VERSION}-experimental-${TRAVIS_BRANCH}
 	fi
 }
+
+
+get_azureenvironment () {
+	if [[ "$TRAVIS_BRANCH" == "master" ]] 
+	then 
+		echo makeen-demo
+	elif [[ "$TRAVIS_BRANCH" == "stable" ]]
+	then 
+		echo makeen-stable
+	elif [[ "$TRAVIS_BRANCH" == "develop" ]]
+	then 
+		echo makeen-develop
+	else 
+		echo makeen-experimental
+	fi
+}
+
