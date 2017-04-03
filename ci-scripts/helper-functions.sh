638532202c899f9b97b791fbe7d96a3114167e5a
Nick <nick@wirestorm.us>
2017-04-03 08:16:20 -0700

    redoing deployment strategy

--- ci-scripts/helper-functions.sh
+++ ci-scripts/helper-functions.sh
@@ -15,3 +15,18 @@ get_fulldockertag () {
 		echo experimental-${TRAVIS_BRANCH}:${VERSION}
 	fi
 }
+
+get_dockerbranchtag () {
+	if [[ "$TRAVIS_BRANCH" == "master" ]] 
+	then 
+		echo latest
+	elif [[ "$TRAVIS_BRANCH" == "stable" ]]
+	then 
+		echo stable:${VERSION}
+	elif [[ "$TRAVIS_BRANCH" == "develop" ]]
+	then 
+		echo develop
+	else 
+		echo experimental-${TRAVIS_BRANCH}
+	fi
+}
