859687a97c4c9c1495b938c30d985c8e84ce9278
Victor Zamfir <zamfir.victor@gmail.com>
2017-04-13 17:36:54 +0300

    moved watch and build scripts to makeen-cli

---
+++ packages/cli/src/scripts/watch.js
@@ -0,0 +1,40 @@
+import chokidar from 'chokidar';
+import fs from 'fs';
+import fsp from 'fs-promise';
+import { argv } from 'yargs';
+import {
+  compileFile,
+  getBuildDestinationPath,
+  makeConfig,
+} from '../libs/utils';
+
+const run = async (
+  {
+    watchGlob,
+    packagesDir,
+    srcDir,
+    buildDir,
+    babelConfig,
+  },
+) => {
+  const watcher = chokidar.watch(watchGlob, {
+    ignoreInitial: true,
+  });
+
+  watcher.on('add', file =>
+    compileFile({ file, babelConfig, packagesDir, srcDir, buildDir }));
+  watcher.on('change', file =>
+    compileFile({ file, babelConfig, packagesDir, srcDir, buildDir }));
+  watcher.on('unlink', srcPath =>
+    fs.unlinkSync(
+      getBuildDestinationPath({ srcPath, packagesDir, srcDir, buildDir }),
+    ));
+  watcher.on('unlinkDir', srcPath =>
+    fsp.remove(
+      getBuildDestinationPath({ srcPath, packagesDir, srcDir, buildDir }),
+    ));
+
+  return watcher;
+};
+
+run(makeConfig(argv));
