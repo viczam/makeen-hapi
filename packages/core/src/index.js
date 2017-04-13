d7460b008511acaecf904bee931fda1c91216d52
Victor Zamfir <zamfir.victor@gmail.com>
2017-04-13 17:34:32 +0300

    server factory is part of the code now

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -5,6 +5,7 @@ import { Store, decorators } from 'octobus-mongodb-store';
 import MessageBus from './octobus/MessageBus';
 import ServiceBus from './octobus/ServiceBus';
 import pkg from '../package.json';
+import createServer from './libs/createServer';
 
 const MongoDbStore = decorators.withTimestamps(Store);
 
@@ -19,7 +20,7 @@ const register = async (server, options, next) => {
     const { mongoDb, refManager } = server.plugins['makeen-db'];
     const messageBus = new MessageBus();
 
-    messageBus.onMessage((msg) => {
+    messageBus.onMessage(msg => {
       console.log(JSON.stringify(msg, null, 2)); // eslint-disable-line
     });
 
@@ -40,13 +41,15 @@ const register = async (server, options, next) => {
       return serviceBus;
     });
 
-    server.method('createStore', (config = {}) => (
-      new MongoDbStore({
-        db: mongoDb,
-        refManager,
-        ...config,
-      })
-    ));
+    server.method(
+      'createStore',
+      (config = {}) =>
+        new MongoDbStore({
+          db: mongoDb,
+          refManager,
+          ...config,
+        }),
+    );
 
     next();
   } catch (err) {
@@ -59,4 +62,4 @@ register.attributes = {
   dependencies: ['hapi-octobus', 'makeen-db'],
 };
 
-export { register };
+export { register, createServer };
