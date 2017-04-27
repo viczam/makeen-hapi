f5b2845c2d789be4a106c1136ded8abd4963ccd6
Nick <nick@wirestorm.us>
2017-04-27 13:30:02 -0700

    it's a function, not a variable, stupid!

--- ci-scripts/deploy.sh
+++ ci-scripts/deploy.sh
@@ -20,7 +20,7 @@ fi
 #az login -u ${AZURE_USER} -p ${AZURE_PASSWORD}
 #az account set --subscription ${AZURE_SUBSCRIPTION}
 
-echo "export MAKEEN_DOCKER_TAG=${get_fulldockertag}" >> deploy.env
+echo "export MAKEEN_DOCKER_TAG=$(get_fulldockertag)" >> deploy.env
 scp -o "StrictHostKeyChecking no" docker-deploy.yml makeen@$(get_azureenvironment).cloudapp.net:/home/makeen/
 scp -o "StrictHostKeyChecking no" deploy.env makeen@$(get_azureenvironment).cloudapp.net:/home/makeen/deploy.env
 ssh -o "StrictHostKeyChecking no" makeen@$(get_azureenvironment).cloudapp.net 'source deploy.env; docker-compose -f docker-deploy.yml -p makeen up -d'
